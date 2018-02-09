c5064899695f68d6e2517db50477c4b3f1cb6421
Slade Utley <slade.utley@gmail.com>
2018-02-08 19:15:10 -0600

    syling to make nav bar and links nicer

--- app/controllers/CategoryCtrl.js
+++ app/controllers/CategoryCtrl.js
@@ -23,8 +23,7 @@ angular
       console.log("a new goal was added", $scope.goalItems);
       $scope.goalItems.uid = firebase.auth().currentUser.uid;
       //COULD I USE A "firebase.auth().currentUser.uid" below in "getUserGoals" to get user id???
-      GoalFactory.addNewGoal($scope.goalItems)
-      .then(data => {
+      GoalFactory.addNewGoal($scope.goalItems).then(data => {
         // $location.location.href = "#!/user-page";
         $route.reload();
         //   $window.location.href = "#!/user-page";
@@ -49,25 +48,31 @@ angular
     //updating goals
 
     //TO DO: REVERT BACK TO FALSE WHEN UNCLICKED
-    $scope.isGoalAccomplished = (goalId) => {
-        $scope.setAccomplished = {accomplished: true};        
-        console.log('$scope.setAccomplished',$scope.setAccomplished);
-        GoalFactory.updateGoal(goalId, $scope.setAccomplished)
-        .then(() => {
-            GoalFactory.getUserGoals();
-            // $route.reload();
-        });
+    $scope.isGoalAccomplished = goalId => {
+      //for some reason, couldn't pass in $scope.goalList.accomplished and had to create new object with same name(accomplished) to replace the old one and change it to true
+      //PROBABLY NOT THE RIGHT WAY TO DO THIS
+      let checkbox = document.getElementById("checkbox");
+      if (checkbox.checked) {
+        console.log("goal checked");
+        $scope.setAccomplished = { accomplished: true };
+      } else {
+        $scope.setAccomplished = { accomplished: false };
+        console.log("goal unchecked");
+      }
+      GoalFactory.updateGoal(goalId, $scope.setAccomplished).then(() => {
+        GoalFactory.getUserGoals();
+        // $route.reload();
+      });
     };
 
-    //write a function that when user reloads page, it loads the checked items
+    //write a function that when user reloads page, it loads the unchecked items first, then the checked. right now the check mark gets erased
 
     //deleting goals
 
     $scope.deleteUserGoal = goalId => {
-      GoalFactory.deleteGoal(goalId)
-      .then(() => {
-          GoalFactory.getUserGoals();
-          $route.reload();
+      GoalFactory.deleteGoal(goalId).then(() => {
+        GoalFactory.getUserGoals();
+        $route.reload();
       });
     };
   });
